474d14f6cc0d5ceefa10bcb05f9e128bdcd5129d
Donat Imeri <donatimmeri@gmail.com>
2019-04-22 00:42:16 +0200

    Firebase Storage added, DecisionsTab changed, gradle changed

--- app/src/main/java/com/dright/MyDecisionsTab.java
+++ app/src/main/java/com/dright/MyDecisionsTab.java
@@ -18,10 +18,13 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +32,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,14 +46,18 @@ public class MyDecisionsTab extends Fragment {
     private CheckBox chbAnonymous;
     private LinearLayout addOption, optionsLayout;
     private EditText dilemaDescription;
-    private SeekBar sbPriority, sbTimeout;
+    private SeekBar sbPriority;
+    private Switch isImageOption;
     private TextView lblDocents, txtPriority, txtTimeout, lblError;
+    private NumberPicker pckMinutes, pckHours, pckDays;
     private int optionCounter;
     public Uri imageUri;
     private View currentView;
     private Docent userDocents;
     private FirebaseDatabase fb;
     private FirebaseAuth auth;
+    private FirebaseStorage fbStorage;
+    private StorageReference imageRef;
 
     @Nullable
     @Override
@@ -69,22 +79,27 @@ public class MyDecisionsTab extends Fragment {
         optionsLayout=(LinearLayout) activity.findViewById(R.id.options_layout);
         dilemaDescription=(EditText) activity.findViewById(R.id.txt_dilema_description);
         sbPriority=(SeekBar) activity.findViewById(R.id.sb_priority);
-        sbTimeout=(SeekBar) activity.findViewById(R.id.sb_timeout);
+        isImageOption=(Switch)activity.findViewById(R.id.switch_is_image);
         lblDocents=(TextView) activity.findViewById(R.id.lbl_total_docent);
         txtPriority=(TextView) activity.findViewById(R.id.lbl_priority_value);
-        txtTimeout=(TextView) activity.findViewById(R.id.lbl_timout_type);
         lblError=(TextView) activity.findViewById(R.id.lbl_dilema_error);
+        pckMinutes=(NumberPicker)activity.findViewById(R.id.pick_minutes);
+        pckMinutes.setMinValue(0); pckMinutes.setMaxValue(59);
+        pckHours=(NumberPicker)activity.findViewById(R.id.pick_hours);
+        pckHours.setMinValue(0); pckHours.setMaxValue(23);
+        pckDays=(NumberPicker)activity.findViewById(R.id.pick_days);
+        pckDays.setMinValue(0); pckDays.setMaxValue(30);
         btnSubmit = (FloatingActionButton) activity.findViewById(R.id.btn_submit);
+        userDocents=null;
         fb=FirebaseDatabase.getInstance();
         auth=FirebaseAuth.getInstance();
-        userDocents=null;
+        fbStorage=FirebaseStorage.getInstance();
+        imageRef=fbStorage.getReference("dilema_photos");
 
-        addOption(optionsLayout, "Option "+optionCounter, imageUri);
-
+       // addOption(optionsLayout, "Option "+optionCounter, imageUri);
 
         //Get users docents
         final DatabaseReference userDocentsReference=fb.getReference("Users/"+auth.getUid()+"/docents");
-        Log.d("userId","aaaa"+auth.getUid());
         userDocentsReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -126,28 +141,14 @@ public class MyDecisionsTab extends Fragment {
 
             }
         });
-        sbTimeout.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                txtTimeout.setText(timeoutText(progress));
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
         addOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addOption(optionsLayout,"Option "+optionCounter, imageUri);
             }
         });
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -160,8 +161,8 @@ public class MyDecisionsTab extends Fragment {
                 }
 
                 Dilema newDilema=new Dilema(String.valueOf(dilemaDescription.getText()), listOptions(optionsLayout),
-                        null, auth.getUid(), sbPriority.getProgress(), sbTimeout.getProgress(),
-                        checkAnonimity(), listOptionResults);
+                        null, auth.getUid(), sbPriority.getProgress(), calculateTimeout(),
+                        checkAnonimity(), listOptionResults, checkTextImage());
 
                 DatabaseReference table=fb.getReference("Dilema");
                 DatabaseReference newRow=table.push();
@@ -182,6 +183,14 @@ public class MyDecisionsTab extends Fragment {
             r.onClick(null);
             Uri imageData=data.getData();
             imageUri=imageData;
+            StorageReference newImage=imageRef.child(imageData.getLastPathSegment());
+            newImage.putFile(imageUri).addOnSuccessListener(this.getActivity(), new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri newImageStored=taskSnapshot.getUploadSessionUri();
+                    Log.d("newImage","aaa"+newImageStored.toString());
+                }
+            });
             addOption(optionsLayout,"Option "+currentIndex, imageUri);
             imageUri=null;
         }
@@ -200,17 +209,20 @@ public class MyDecisionsTab extends Fragment {
             default: return "None";
         }
     }
-    private String timeoutText(int progress){
-        switch (progress){
-            case 0: return "3 minutes";
-            case 1: return "10 minutes";
-            case 2: return "1 hour";
-            case 3: return "6 hours";
-            case 4: return "1 day";
-            case 5: return "1 week";
-            case 6: return "1 month";
-            default: return "None";
+    private long calculateTimeout(){
+        int days=pckDays.getValue();
+        int hours=pckHours.getValue();
+        int minutes=pckMinutes.getValue();
+
+        int totalTimeInMinutes=((hours*60)*days)+(hours*60)+minutes;
+        return totalTimeInMinutes;
+    }
+    private boolean checkTextImage(){
+        if (isImageOption.getTextOff().equals("OFF")){
+            return false;
         }
+        else
+            return true;
     }
     private boolean checkAnonimity(){
        return chbAnonymous.isChecked();
